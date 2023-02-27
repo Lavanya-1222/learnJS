b8db38e110efc19ba10ef4ac60bd2bf475e65cc8
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-02-27 15:35:19 +0530

    RestOperator

---
+++ RestOperator.js
@@ -0,0 +1,11 @@
+const a=(name,...marks)=>{
+let sum=0;
+    
+    for (let i in marks){
+        sum+=marks[i];
+
+    }
+    console.log(name,sum)
+    console.log(marks)
+}
+a('lavanya',90,98,87)
