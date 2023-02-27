6d5aa1150c20f9daec924eb4608b29b8f880a4ec
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-02-27 15:03:43 +0530

    ArrowFunction.js

---
+++ ArrowFunction.js
@@ -0,0 +1,9 @@
+let a=()=>console.log("hello :)");
+a();
+let b=name=>`Welcome ${name}`;
+console.log(b("lavanya"));
+let c=(fname,lname)=>{
+    console.log(`Welcome ${fname} ${lname}`);
+    console.log(':)');
+}
+c("lava","mira");
