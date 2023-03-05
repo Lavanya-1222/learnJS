4b7f3b36936347338d64908a9d9633830b76bb8e
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-03-05 17:13:32 +0530

    strictmode

---
+++ strictmode.js
@@ -0,0 +1,28 @@
+// a=10
+// console.log(a)
+
+
+// "use strict"
+// a=10      
+// console.log(a)
+
+
+// function ab(a,b,b){
+//     console.log(a+b+b);
+// }
+// ab(10,20,40);
+//result is 90
+
+// "use strict"
+// function ab(a,b,b){
+//     console.log(a+b+b);
+// }
+// ab(10,20,40);
+// error
+
+"use strict"
+function ab(a,b,c){
+    console.log(a+b+c);
+}
+ab(10,20,40);
+//no error
