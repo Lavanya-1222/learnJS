c3e1b037dd9a150e2809cd91e524694bd7078d49
Lavanya Miranam <83878776+Lavanya-1222@users.noreply.github.com>
2023-03-05 11:44:25 +0530

    Module

---
+++ file1.js
@@ -0,0 +1,10 @@
+// export let name="lava"
+// export let a=()=>"am function"
+
+
+// export default ()=>"am function"
+// let name="lavanya"
+// export {name}
+
+export {name} from './file3.js'
+// export {name}
---
+++ file2.js
@@ -0,0 +1,18 @@
+// import{name,a} from './file1.js'
+// console.log(name);
+// console.log(a())
+
+// import * as lava from './file1.js';
+// console.log(lava.name)
+// console.log(lava.a())
+
+
+// // import {default as lava} from './file1.js' or
+// import lava from './file1.js'
+// import {name} from './file1.js'
+// console.log(lava())
+// console.log(name);
+
+import {name} from './file1.js'
+console.log(name)
+
