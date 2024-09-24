3e98f7659fadb520e7080a22fa9324d66997ad2e
Lavanya-1222 <lavanyan1205@gmail.com>
2024-09-24 20:45:30 +0530

    all String functions get completed today

--- String.js
+++ String.js
@@ -2,270 +2,178 @@ let name='lava';
 
 let name1= new String('lava')
 console.log(name1)// it prints [String: 'lava']
-console.log(typeof(name)
-)
-console.log(typeof(name1))
+console.log(typeof(name))//type is string
+console.log(typeof(name1))//type is object
 
 
-console.log(name===name1);
+console.log(name===name1);//false
 
 
 // ------------------methods----------------------------
 string1='Lavanya miranam'
-// -------------slice------------------
+console.log("-------------slice------------------")
+console.log(string1.slice(0))
 console.log(string1.slice(3,9))
-// -------------charAt------------------
-console.log(string1.charAt(3))
-//-------------indexOf------lastIndexOf------------
-console.log("lastindexOf",string1.lastIndexOf('an'))
+console.log("slice by negative value",string1.slice(-1))//it return last char
+console.log(string1.slice(-3))//it return 3 char from last
+console.log(string1.slice(-3,-1))//it will perform like [)
+console.log(string1.slice(16,5))// it gives you empty charecter if start>stop
 
 
-console.log(string1.substring(-3,7))
+console.log("-------------substring------------------------------")
+console.log(string1.substring(1,3))
+console.log(string1.substring(-3))//If any argument is negative or is NaN, it is treated as 0.
+console.log(string1.substring(14,4))//If start > stop, then the function swaps both arguments.
 
-console.log(string1.substr(1,3))
 
+console.log("-------------charAt-------------------------")
+console.log(string1.charAt(3))//charecter at this position starts from 0
+
+console.log("----------------lastIndexOf------------")
+console.log("lastindexOf",string1.lastIndexOf('an'))// it gives you last index of duplicate element present
+
+
+console.log("-----------------------substr-------------------------------------")
+console.log(string1.substr(1,3))// start from length (how many charecters)
+
+console.log("---------------------------toLowerandUPPer---------------------------------")
 console.log(string1.toLowerCase(),string1.toUpperCase())
+
+console.log("-----------------------includes-------------------------------------")
 console.log(string1.includes("Lavanya"))//it finds the string is present or not
+                                        //never take regular expresion /fhg/
+
+console.log("-------------------startsWith---------endsWith--------------------------------")
 console.log(string1.startsWith("Lava1")) 
 console.log(string1.endsWith("nam")) 
-console.log(string1.search("an",4))
-console.log(string1.search(/an/))//it works 
-console.log(string1.indexOf(/an/))//it gives you -1 
-/*IndexOf() - it accepts string literals or string objects but not regular expressions. 
-It also accepts a zero-based integer value to start its search from, e.g.:
 
+
+console.log("-------------------serach-----------------------------------------")
+console.log(string1.search("an",4))//  But it doesn't accepts a index to start the search from.
+console.log(string1.search(/an/))//Search() - accepts both string literals or string objects and regular expressions.
+
+
+console.log("-------------------indexOf-----------------------------------------")
+console.log(string1.indexOf(/an/))//it accepts string literals or string objects but not regular expressions.  
+//It also accepts a zero-based integer value to start its search from, e.g.:
 //"babyelephant".indexOf("e"); // gives you 4
-"babyelephant".indexOf("e",5); // gives you 6 as the search starts from 6th position or 5th index.
-var m= /e/; "babyelephant".indexOf(m); //gives -1 as it doesnt accepts regular expressions.
+console.log(string1.indexOf("an",4));//it accepts starts integer value
 
-Search() - accepts both string literals or string objects and regular expressions.
- But it doesn't accepts a index to start the search from.
-*/
+
+
+
+console.log("-----------------------match-------------------------------------")
 s="lavANya miranam"
+
 console.log(string1.match(/an/g))
 console.log(s.match(/An/gi))
 console.log(string1.match(/lava/i))
 
 //The match() method in JavaScript is used for identifying 
 //and retrieving substrings that fit a specified pattern, defined by a regular expression. 
-//The match() method in JavaScript is used for identifying and 
-//retrieving substrings that fit a specified pattern, defined by a regular expression. 
+//and return an array at output  
 //g is for every place match and i is for ignore casesencitiveness //gi is for both
 
-console.log(string1.replace("Lava","vava"),' ',string1)
-console.log(string1.replace(/an/g,"sm"))
-// JavaScript replace() method is used for manipulating strings. It allows you to search for a specific
-// part of a string, called a substring, and then replace it with another substring.
-//it does not changes the original string
 
+console.log("----------------------------replace--------------------------------")
+console.log(string1.replace("Lava","vava"))
+console.log(string1.replace(/an/g,"sm"))//it will replace all over the string
+
+
+console.log("-------------------------------trim-----------------------------")
 
 s2="            lavanya miranam       "
-console.log("trim method=",s2.trim())
+console.log("trim method=",s2.trim())//it remove spaces
+console.log("trim method=",s2.trimEnd())
+console.log("trim method=",s2.trimStart())
+
+console.log("--------------charCodeAt-------------fromCharCode---------------------------------")
+console.log(s2.charCodeAt(1))// it will return ASCII Code
+console.log(String.fromCharCode(83))// it return the value related string 
 
 
+console.log("--------------------concat----------------------------------------")
+console.log(s.concat(string1,s2))//you can concate two or more variables
+
+console.log("--------------------------split----------------------------------")
+console.log(string1.split("a"))//split by char you can give space also
+
+console.log("--------------------------repeat----------------------------------")
+console.log(string1.repeat(3))//it repeats number of times 
+
+a=true;
+a1=89
+console.log("--------------------------toString----------------------------------")
+console.log(a.toString(),typeof a,typeof a.toString() )
+console.log(a1.toString(),typeof a1,typeof a1.toString() )// it converts to string 
+
+console.log("--------------------------valueOf----------------------------------")
+console.log(string1.valueOf())// it will return value
 // String Functions all 
 //String {'', anchor: ƒ, at: ƒ, big: ƒ, blink: ƒ, …}
-// anchor
-// : 
-// ƒ anchor()
-// at
-// : 
-// ƒ at()
-// big
-// : 
-// ƒ big()
-// blink
-// : 
-// ƒ blink()
-// bold
-// : 
-// ƒ bold()
-// charAt
-// : 
-// ƒ charAt()
-// charCodeAt
-// : 
-// ƒ charCodeAt()
-// codePointAt
-// : 
-// ƒ codePointAt()
-// concat
-// : 
-// ƒ concat()
-// constructor
-// : 
-// ƒ String()
+ //anchor
+//at()
+//big()
+//blink()
+//bold()
+//charAt()
+// charCodeAt()
+// codePointAt()
+// concat()
 // endsWith
-// : 
-// ƒ endsWith()
-// fixed
-// : 
-// ƒ fixed()
-// fontcolor
-// : 
-// ƒ fontcolor()
-// fontsize
-// : 
-// ƒ fontsize()
-// includes
-// : 
-// ƒ includes()
-// indexOf
-// : 
-// ƒ indexOf()
+//fixed()
+//fontcolor()
+// fontsize()
+//includes()
+// indexOf()
 // isWellFormed
-// : 
-// ƒ isWellFormed()
 // italics
-// : 
-// ƒ italics()
 // lastIndexOf
-// : 
-// ƒ lastIndexOf()
 // length
-// : 
-// 0
 // link
-// : 
-// ƒ link()
 // localeCompare
-// : 
-// ƒ localeCompare()
 // match
-// : 
-// ƒ match()
-// matchAll
-// : 
-// ƒ matchAll()
+//matchAll
 // normalize
-// : 
-// ƒ normalize()
 // padEnd
-// : 
-// ƒ padEnd()
 // padStart
-// : 
-// ƒ padStart()
 // repeat
-// : 
-// ƒ repeat()
 // replace
-// : 
-// ƒ replace()
 // replaceAll
-// : 
-// ƒ replaceAll()
 // search
-// : 
-// ƒ search()
 // slice
-// : 
-// ƒ slice()
 // small
-// : 
-// ƒ small()
 // split
-// : 
-// ƒ split()
 // startsWith
-// : 
-// ƒ startsWith()
 // strike
-// : 
-// ƒ strike()
 // sub
-// : 
-// ƒ sub()
 // substr
-// : 
-// ƒ substr()
 // substring
-// : 
-// ƒ substring()
 // sup
-// : 
-// ƒ sup()
 // toLocaleLowerCase
-// : 
-// ƒ toLocaleLowerCase()
 // toLocaleUpperCase
-// : 
-// ƒ toLocaleUpperCase()
 // toLowerCase
-// : 
-// ƒ toLowerCase()
 // toString
-// : 
-// ƒ toString()
 // toUpperCase
-// : 
-// ƒ toUpperCase()
 // toWellFormed
-// : 
-// ƒ toWellFormed()
-// trim
-// : 
 // ƒ trim()
-// trimEnd
-// : 
-// ƒ trimEnd()
 // trimLeft
-// : 
-// ƒ trimStart()
 // trimRight
-// : 
-// ƒ trimEnd()
+// trimEnd()
 // trimStart
-// : 
-// ƒ trimStart()
 // valueOf
-// : 
-// ƒ valueOf()
-// Symbol(Symbol.iterator)
-// : 
-// ƒ [Symbol.iterator]()
 // [[Prototype]]
-// : 
 // Object
-// constructor
-// : 
-// ƒ Object()
 // hasOwnProperty
-// : 
-// ƒ hasOwnProperty()
 // isPrototypeOf
-// : 
-// ƒ isPrototypeOf()
 // propertyIsEnumerable
-// : 
-// ƒ propertyIsEnumerable()
 // toLocaleString
-// : 
-// ƒ toLocaleString()
 // toString
-// : 
-// ƒ toString()
 // valueOf
-// : 
-// ƒ valueOf()
 // __defineGetter__
-// : 
-// ƒ __defineGetter__()
 // __defineSetter__
-// : 
-// ƒ __defineSetter__()
 // __lookupGetter__
-// : 
-// ƒ __lookupGetter__()
 // __lookupSetter__
-// : 
-// ƒ __lookupSetter__()
 // __proto__
-// : 
-// (...)
 // get __proto__
-// : 
-// ƒ __proto__()
-// set __proto__
-// : 
-// ƒ __proto__()
+// set proto__()
