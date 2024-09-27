e41f8ea0b3022558b970f7668950c61782bc1669
Lavanya-1222 <lavanyan1205@gmail.com>
2024-09-27 20:51:53 +0530

    switch file id added

---
+++ switch.js
@@ -0,0 +1,49 @@
+console.log("----------------------------------By Number-------------------------------------");
+
+
+let num=90
+switch(num){
+case 90:console.log("90");
+break;
+case 100:console.log("100");
+break;
+default:
+    console.log("if not excuted")
+
+
+}
+
+
+console.log("----------------------------------By String-------------------------------------");
+
+let string="lavanya"
+
+switch(string)
+{
+case "lavanya":console.log("Like you lavanya, you can achive it");
+break;
+case "Saranya":console.log("hi saranya");
+break;
+default:
+    console.log("lavanya")
+
+
+
+}
+
+console.log("----------------------------------Multi;ple cases added -------------------------------------");
+let day=1
+
+switch(day)
+
+{
+    case 0: case 1:
+    console.log("Monday");
+break;
+    case 2:
+        console.log("tuesday")
+        break;
+
+    default:
+        console.log("no number entered")
+}
