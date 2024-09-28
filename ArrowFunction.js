0d0b0c6956347df283e7b6a79f02872385e72821
Lavanya-1222 <lavanyan1205@gmail.com>
2024-09-28 17:57:43 +0530

    added new concepts

--- ArrowFunction.js
+++ ArrowFunction.js
@@ -1,15 +1,18 @@
+console.log("----------1Line Code---------- ")
 let a=()=>console.log("hello :)");
 a();
-console.log("----------1Line Code---------- ")
-let b=name=>`Welcome ${name}`;
-console.log("----------1argument  with return---------- ")
-console.log(b("lavanya"));
 
-console.log("----------Multiple line and arguments----------")
+console.log("----------1argument  without return keyword ---------- ")
+let b=name=>`Welcome ${name}`;// if you writing  in one line no need of write return keyword use () brackets for returning objs
+console.log(b("lavanya"));   // if you writing in {} multiple line code then you have to write return keyword
+
+
+console.log("----------Multiple line and arguments-with return keyword ---------")
 let c=(fname,lname)=>{
     console.log(`Welcome ${fname} ${lname}`);
     x= String.fromCodePoint(0x1F60D)
-    console.log(x);
+    // console.log(x);
+    return x
 }
 
-c("lava","mira");
+console.log(c("lava","mira"));
